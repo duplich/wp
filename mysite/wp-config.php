5b22e124f151cd716d5db6651a87f7dc55e9adff
duplich <j@rab.eu>
2013-12-05 16:51:08 +0100

    update settings in wp-config.php

--- mysite/wp-config.php
+++ mysite/wp-config.php
@@ -13,6 +13,15 @@
  *
  * @package WordPress
  */
+// Tell wordpress where our wordpress package is/ where site should be routed
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+// Define where the wp-content is
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+
+// Define the Default theme
+define('WP_DEFAULT_THEME', 'waterfall');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
